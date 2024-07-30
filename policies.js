f1f45e8effdba0836df804e302ec906fe3c40599
AshwiniKala <ashwini.chandu18@gmail.com>
2024-07-30 12:39:01 -0700

    Update policies.js

--- policies.js
+++ policies.js
@@ -2,7 +2,7 @@
  * Enter here the user flows and custom policies for your B2C application
  * To learn more about user flows, visit: https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-overview
  * To learn more about custom policies, visit: https://docs.microsoft.com/en-us/azure/active-directory-b2c/custom-policy-overview
- */
+ */         
 const b2cPolicies = {
     names: {
         signUpSignIn: "B2C_1_susi_reset_v2",
